96fc77dddc1580e48ebcf6e011cd40af7efb26ea
Roger Vinicius <rogervtsc@hotmail.com>
2024-07-01 11:32:19 -0300

    commit inicial

---
+++ colecoes/exemploArray.ts
@@ -0,0 +1,22 @@
+import { readlinkSync } from "fs";
+
+const numeros: Array<number> = new Array<number>();
+
+const numeros2: number[] = [];
+
+numeros.push(1);
+numeros.push(2);
+numeros.push(3);
+numeros.unshift(15, 80, 100);
+
+console.log(numeros);
+console.log(`A posição do elemento 15 é: ${numeros.indexOf(1)}`);
+console.log(`Existe no Array o elemento 1? ${numeros.includes(1)}`);
+
+numeros[numeros.indexOf(15)] = 10;
+
+console.table(numeros);
+
+numeros.splice(numeros.indexOf(80), 1);
+
+console.table(numeros);
